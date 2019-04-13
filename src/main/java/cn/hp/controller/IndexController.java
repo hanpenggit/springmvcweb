3aa34ad3ba4b9efe7bbd35a503fdf8bf7efeeca6
hanpeng <hanpeng.jack@qq.com>
2019-04-13 16:17:41 +0800

    1、添加自定义注解的代码 2、添加了拦截器的示例 3、添加了切面的示例 4、加入事务注解，事务放在Service层

--- src/main/java/cn/hp/controller/IndexController.java
+++ src/main/java/cn/hp/controller/IndexController.java
@@ -1,5 +1,6 @@
 package cn.hp.controller;
 
+import cn.hp.annotations.OperateLog;
 import cn.hp.service.TestService;
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,22 +21,22 @@ public class IndexController {
     public String index() {
         return "index";
     }
-    @RequestMapping("/list")
+    @RequestMapping(value = "/list",produces = "text/html;charset=UTF-8")
     @ResponseBody
+    @OperateLog(value="自定义注解demo")
     public String list(HttpServletRequest req) {
-        return JSON.toJSONString(testService.list());
+        Object obj=testService.list();
+        return JSON.toJSONString(obj);
     }
     @RequestMapping("/insert")
-    @ResponseBody
     public String insert(HttpServletRequest req) {
         String  name=req.getParameter("name");
         Map<String,Object> params=new HashMap<>();
         params.put("name",name);
         Integer result=testService.insert(params);
-        return result.toString();
+        return "redirect:/list";
     }
     @RequestMapping("/update")
-    @ResponseBody
     public String update(HttpServletRequest req) {
         String  id=req.getParameter("id");
         String  name=req.getParameter("name");
@@ -43,15 +44,14 @@ public class IndexController {
         params.put("id",id);
         params.put("name",name);
         Integer result=testService.update(params);
-        return result.toString();
+        return "redirect:/list";
     }
     @RequestMapping("/delete")
-    @ResponseBody
     public String delete(HttpServletRequest req) {
         String  id=req.getParameter("id");
         Map<String,Object> params=new HashMap<>();
         params.put("id",id);
         Integer result=testService.delete(id);
-        return result.toString();
+        return "redirect:/list";
     }
 }
--- src/main/java/cn/hp/service/impl/TestServiceImpl.java
+++ src/main/java/cn/hp/service/impl/TestServiceImpl.java
@@ -1,10 +1,13 @@
 package cn.hp.service.impl;
 
+import cn.hp.annotations.OperateLog;
 import cn.hp.dao.TestDao;
 import cn.hp.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.beans.Transient;
 import java.util.List;
 import java.util.Map;
 
@@ -12,22 +15,57 @@ import java.util.Map;
 public class TestServiceImpl implements TestService {
     @Autowired
     TestDao testDao;
+
+    /**
+     * OperateLog为自定义注解
+     * Transactional注解，为手动加入事务注解，在此方法内如果出现异常，则回滚事务
+     * @return
+     */
     @Override
+    @OperateLog(value="查询test列表")
+    @Transactional
     public List<Map<String, Object>> list() {
         return testDao.list();
     }
 
+    /**
+     * OperateLog为自定义注解
+     * Transactional注解，为手动加入事务注解，在此方法内如果出现异常，
+     * @return
+     */
     @Override
+    @OperateLog(value="增加test")
+    @Transactional
     public int insert(Map<String, Object> params) {
-        return testDao.insert(params);
+        int result=testDao.insert(params);
+        if(result>0){
+            System.out.println(result);
+        }
+        int j=10/0;
+        return result;
     }
 
+    /**
+     * OperateLog为自定义注解
+     * Transactional注解，为手动加入事务注解，在此方法内如果出现异常，则回滚事务
+     * 比如此方法中update执行成功之后，后面除以0，抛出异常，此时，更新回滚
+     * @return
+     */
     @Override
+    @OperateLog(value="更新test")
+    @Transactional
     public int update(Map<String, Object> params) {
-        return testDao.update(params);
+        int result=testDao.update(params);
+        if(result>0){
+            System.out.println(result);
+        }
+        int j=10/0;
+        return result;
     }
 
+    @OperateLog(value="删除test")
     @Override
+    @Transactional
     public int delete(String id) {
         return testDao.delete(id);
     }
